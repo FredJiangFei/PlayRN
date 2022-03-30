b5ed0015a34a41970fded6ee94bde06e769cbc8f
Fred Jiang <jiangf@shinetechchina.com>
2022-03-31 06:36:12 +0800

    Share Image

--- BasicRN/App.js
+++ BasicRN/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import logo from './assets/mario.png';
+import RnImagePicker from './app/components/RnImagePicker';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Text */}
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+
+      {/* Image */}
+      {/* <Image source={logo} style={{ width: 305, height: 159 }} />
+      <Image source={{ uri: "https://picsum.photos/id/237/200/300" }} style={{ width: 305, height: 159 }} /> */}
+    
+      {/* Button */}
+      {/* <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Say Hello</Text>
+      </TouchableOpacity> */}
+
+      {/* ImagePicker */}
+      <RnImagePicker />
+
+
     </View>
   );
 }
---
+++ BasicRN/app/components/RnImagePicker.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'; 
+
+export default function RnImagePicker() {
+    const [selectedImage, setSelectedImage] = React.useState({});
+    const openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+          alert("Permission to access camera roll is required!");
+          return;
+        }
+    
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+      
+        setSelectedImage({ localUri: pickerResult?.uri });
+    }
+
+    let openShareDialogAsync = async () => {
+        if (Platform.OS === 'web') {
+          alert(`Uh oh, sharing isn't available on your platform`);
+          return;
+        }
+    
+        await Sharing.shareAsync(selectedImage.localUri);
+    }; 
+
+  return (
+    <>
+        { selectedImage.localUri && <Image style={styles.thumbnail} source={{ uri: selectedImage.localUri }} /> }
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+            <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+    button: {
+      backgroundColor: "blue",
+      padding: 20,
+      borderRadius: 5,
+      marginBottom: 8
+    },
+    buttonText: {
+      fontSize: 20,
+      color: '#fff',
+    }, 
+    thumbnail: {
+        width: 300,
+        height: 300,
+        resizeMode: "contain"
+    }
+  });
